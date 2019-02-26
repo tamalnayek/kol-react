b447e6405b96225a76b4195a58b218be3364e6fe
nagabhushanamn <nagabhushanamn@gmail.com>
2019-02-26 15:53:52 +0530

    todo app

--- 1-play-with-js/index.js
+++ 1-play-with-js/index.js
@@ -27,19 +27,22 @@ class TodoService {
         this.todos = this.todos.concat(newTodo);
     }
     editTodo(id, newTitle) {
-        //..
+        this.todos = this.todos.map(todo => todo.id === id ? { ...todo, title: newTitle } : todo)
     }
     completeTodo(id) {
-        //.
+        this.todos = this.todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
     }
     deleteTodo(id) {
-        //
+        this.todos = this.todos.filter(todo => todo.id !== id)
     }
     completeAll() {
-        //..
+        let isAllCompleted = this.todos.every(todo => todo.completed);
+        this.todos = this.todos.map(todo => {
+            return { ...todo, completed: !isAllCompleted }
+        })
     }
     clearCompleted() {
-
+        this.todos = this.todos.filter(todo => !todo.completed)
     }
     display(filter) {
         //..
